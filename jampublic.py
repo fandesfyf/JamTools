234034060e6a47e997ca155e070f0ca3f0ad9aad
fandesfyf <2861114322@qq.com>
2023-03-07 21:24:32 +0800

    fix:修复没有离线fake_ua文件时报错

--- jampublic.py
+++ jampublic.py
@@ -47,7 +47,11 @@ apppath = get_apppath()
 def get_UserAgent():
     ua = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36 Edg/108.0.1462.54"
     try:
-        ua = UserAgent(path=os.path.join(apppath,"fake_useragent_0.1.11.json"),verify_ssl=False).random
+        ua_file = os.path.join(apppath,"fake_useragent_0.1.11.json")
+        if os.path.exists(ua_file):
+            ua = UserAgent(path=os.path.join(apppath,"fake_useragent_0.1.11.json"),verify_ssl=False).random
+        else:
+            ua = UserAgent(verify_ssl=False).random
     except Exception as e:
         print(e,"get_UserAgent")
     return ua
