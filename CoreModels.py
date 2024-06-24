6bcd8463babd3a399406baf5290107254bd22441
FanYunFu <2861114322@qq.com>
2024-06-25 01:03:05 +0800

    fix:windows下不使用剪切板历史，自带的就很好用

--- CoreModels.py
+++ CoreModels.py
@@ -1355,10 +1355,10 @@ class JamToolsWindow(QMainWindow):
 
         if self.settings.value('right_ocr', True, bool):
             self.openlistenmouse()
-        
-        documents_path = QStandardPaths.writableLocation(QStandardPaths.DocumentsLocation)
-        clipboard_history_dir = os.path.join(documents_path, 'clipboard_history.json')
-        self.keyboard_mamager = ClipboardManager(history_path=clipboard_history_dir)
+        if sys.platform != "win32": # windows自带的剪切板历史就挺好用了
+            documents_path = QStandardPaths.writableLocation(QStandardPaths.DocumentsLocation)
+            clipboard_history_dir = os.path.join(documents_path, 'clipboard_history.json')
+            self.keyboard_mamager = ClipboardManager(history_path=clipboard_history_dir)
         
         
     def init_others(self):
